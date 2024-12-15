d77e8e0b8fd678a5cc7e0949c8a624dc7c7c6407
Gois <GoisEstudo@gmail.com>
2024-12-15 20:42:43 -0300

    primeiro commit

---
+++ src/main/java/com/bielsoft/desafioForumHubONE/DesafioForumHubOneApplication.java
@@ -0,0 +1,13 @@
+package com.bielsoft.desafioForumHubONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioForumHubOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioForumHubOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bielsoft/desafioForumHubONE/DesafioForumHubOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bielsoft.desafioForumHubONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForumHubOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
